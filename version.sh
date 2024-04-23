84f8af21712950e2d5367aea26a37f13bb8a2073
Jesibu <jbures@codebrewery.de>
2024-04-23 14:07:24 +0200

    fix: version script and backmerge

--- version.sh
+++ version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mv custom_components/*/manifest.json custom_components/twitch_event_sub/temp.json
-jq -r '.name |= "$1"' $GITHUB_WORKSPACE/custom_components/twitch_event_sub/temp.json > $GITHUB_WORKSPACE/custom_components/twitch_event_sub/manifest.json
+VAR="$1"
+jq --arg version "$1" -r '.version |= $version' custom_components/twitch_event_sub/temp.json > custom_components/twitch_event_sub/manifest.json
 rm custom_components/*/temp.json
