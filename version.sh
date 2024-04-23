4bc7a8d0d8150ea64883a4c224512ce23246ab54
Jesibu <jbures@codebrewery.de>
2024-04-23 10:35:35 +0200

    ci: set env vars

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mv custom_components/*/manifest.json custom_components/*/temp.json
-jq -r '.name |= "$1"' custom_components/*/temp.json > custom_components/*/manifest.json
+jq -r '.name |= "$1"' $GITHUB_WORKSPACE/custom_components/*/temp.json > $GITHUB_WORKSPACE/custom_components/*/manifest.json
 rm custom_components/*/temp.json
