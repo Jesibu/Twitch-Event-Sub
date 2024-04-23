98461e486cdf9dcd5009bfafb0a24682c244dfd8
Jesibu <jbures@codebrewery.de>
2024-04-23 10:37:49 +0200

    set fixed path

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mv custom_components/*/manifest.json custom_components/*/temp.json
-jq -r '.name |= "$1"' $GITHUB_WORKSPACE/custom_components/*/temp.json > $GITHUB_WORKSPACE/custom_components/*/manifest.json
+mv custom_components/*/manifest.json custom_components/twitch_event_sub/temp.json
+jq -r '.name |= "$1"' $GITHUB_WORKSPACE/custom_components/twitch_event_sub/temp.json > $GITHUB_WORKSPACE/custom_components/twitch_event_sub/manifest.json
 rm custom_components/*/temp.json
