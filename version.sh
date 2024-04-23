aeee98cdd23f2e2c7f2a9c1b39b8bfb4f52e2212
Jesibu <jbures@codebrewery.de>
2024-04-23 14:11:23 +0200

    fix: remove unused var from varsion.sh

--- version.sh
+++ version.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 mv custom_components/*/manifest.json custom_components/twitch_event_sub/temp.json
-VAR="$1"
 jq --arg version "$1" -r '.version |= $version' custom_components/twitch_event_sub/temp.json > custom_components/twitch_event_sub/manifest.json
 rm custom_components/*/temp.json
